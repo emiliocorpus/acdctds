8198ed29a682a39c8963a97c5527ae645b70da7b
emiliocorpus <emiliocorpus@gmail.com>
2016-08-08 17:47:07 -0700

    completes basic layout of app

--- app/assets/javascripts/components/app.es6.jsx
+++ app/assets/javascripts/components/app.es6.jsx
@@ -1,15 +1,60 @@
 class App extends React.Component {
   render () {
     return (
-      <div className="row centered debugger-green">
-        <div className="content debugger-blue">
-          <h1>AC/DC TD'S</h1>
-          <div className="row debugger-red centered">
-            <div className="col-md-4 debugger-green">Total TD'S: </div>
-            <div className="col-md-4 debugger-green">Total Games: {this.props.totalGames}</div>
-            <div className="col-md-4 debugger-green">Pace: 44</div>
-          </div>
+      <div className="centered main-content">
+
+        <div className="links-container">
+
+          <a href="#">
+            <div className="link debugger-green">
+              AC DC
+            </div>
+          </a>
+
+          <a href="#">
+            <div className="link debugger-green">
+              Coop
+            </div>
+          </a>
+
+          <a href="#">
+            <div className="link debugger-green">
+              Carr
+            </div>
+          </a>
         </div>
+
+        <div className="container individual-content">
+
+          <h1>AC/DC TD'S</h1>
+
+          <div className="row debugger-red">
+            
+
+            <div className="col-md-4">
+
+                <div className="row">
+                  <h3>Total TD'S: </h3>
+                </div>
+
+                <div className="row">
+                  <h3>Total Games: {this.props.totalGames}</h3>
+                </div>
+
+                <div className="row">
+                  <h3>Pace: 44</h3>
+                </div>
+
+            </div>
+
+            <div className="col-md-8 image">
+              <img src="assets/ac-dc.jpg" className="content-image" />
+            </div>
+
+          </div>
+
+        </div>
+
       </div>
     );
   }
