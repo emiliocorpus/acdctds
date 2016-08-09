e361c74ae08b69fe375b35f13a3920ea2f3ab766
emiliocorpus <emiliocorpus@gmail.com>
2016-08-08 17:01:21 -0700

    starts basic templating

--- app/assets/javascripts/components/app.es6.jsx
+++ app/assets/javascripts/components/app.es6.jsx
@@ -1,14 +1,43 @@
 class App extends React.Component {
   render () {
     return (
-      <div>
-        <h1>Test Push to Heroku</h1>
-        <div>Name: {this.props.name}</div>
-        <div>Total Games: {this.props.totalGames}</div>
+      <div className="row centered debugger-green">
+        <div className="content debugger-blue">
+          <h1>AC/DC TD'S</h1>
+          <div className="row debugger-red centered">
+            <div className="col-md-4 debugger-green">Total TD'S: </div>
+            <div className="col-md-4 debugger-green">Total Games: {this.props.totalGames}</div>
+            <div className="col-md-4 debugger-green">Pace: 44</div>
+          </div>
+        </div>
       </div>
     );
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 App.propTypes = {
   name: React.PropTypes.string,
